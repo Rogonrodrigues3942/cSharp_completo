aa1b7e720743de7a5df39345eb9956bd85121c5e
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-08-04 21:00:41 -0300

    Adding new project Problem2. Manipulating methods.

---
+++ projetos/Problem2/Problem2/Product.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Problem2
+{
+    internal class Product
+    {
+        public string ProdName;
+        public int Quantity;
+        public double Price;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Price * Quantity;
+        }
+
+        public void AdicionarProdutos(int quantity)
+        {
+            Quantity += quantity;
+        }
+        public void RemoverProdutos(int quantity)
+        {
+            Quantity -= quantity;
+        }
+
+        public override string ToString()
+        {
+            return "Produto: " + ProdName + 
+                 ", \nR$ " + Price.ToString("F2", CultureInfo.InvariantCulture) +
+                 "\nQuantidade: " + Quantity +
+                "\nValor em estoque: " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ projetos/Problem2/Problem2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Controle de Produto em Estoque - POO");
+
+            Product p = new Product();
+
+            Console.WriteLine("\nInsira os dados do produto.\n\n");
+            Console.Write("Insira o nome do produto: ");
+            p.ProdName = Console.ReadLine();
+            Console.Write("Insira a quantidade: ");
+            p.Quantity = int.Parse(Console.ReadLine());
+            Console.Write("Insira o preço: ");
+            p.Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"\nDados do produto: {p}");
+
+            Console.Write("\nAdiciona novas unidades: ");
+            int quantity = int.Parse(Console.ReadLine()) ;
+            p.AdicionarProdutos(quantity);
+            Console.WriteLine($"\nDados do produto: {p}");
+
+
+            Console.Write("\nRemova unidades: ");
+            quantity = int.Parse(Console.ReadLine()) ;
+            p.RemoverProdutos(quantity);
+            Console.WriteLine($"\nDados do produto: {p}");
+        }
+    }
+}
