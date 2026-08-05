2508781ca36c4cd38a3b214cc82fb96b52d9c518
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-08-04 22:37:13 -0300

    Add new project #xFx41Retangulo, calculate.

---
+++ projetos/ExFx41Retangulo/ExFx41Retangulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace ExFx41Retangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nRetangulo - Geometria - POO\n\n");
+
+            Retangulo retangulo = new Retangulo();
+
+            Console.Write("Enter com valor da altura: ");
+            retangulo.Height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("\nEntre com valor da largurA: ");
+            retangulo.Base = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("");
+            Console.WriteLine($"Area: {retangulo.Area():F2}");
+            Console.WriteLine($"Perimetro: {retangulo.Perimetro():F2}");
+            Console.WriteLine($"Diagonal: {retangulo.Diagonal():F2}");
+        }
+    }
+}
---
+++ projetos/ExFx41Retangulo/ExFx41Retangulo/Retangulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace ExFx41Retangulo
+{
+    class Retangulo
+    {
+        public double Base;
+        public double Height;
+
+        public double Area()
+        {
+            return Base * Height;
+        }
+
+        public double Perimetro()
+        {
+            return ( 2* ( Base +  Height));
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Base,2) + Math.Pow(Height,2));
+        }
+    }
+}
