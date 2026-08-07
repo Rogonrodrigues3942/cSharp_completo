cf5740f04f2e961dcbfae227ee825f2dfa9f9d84
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-08-06 23:01:49 -0300

    Adding new project, goal: currency conversion USD to BRL

---
+++ projetos/StaticConversion/StaticConversion/Conversion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticConversion
+{
+    class Conversion
+    {
+        public static double CurrencyConversion(double quotation, double dolars)
+        {
+            return quotation * (dolars * 1.06);
+        }
+    }
+}
---
+++ projetos/StaticConversion/StaticConversion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace StaticConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nConversor Dólar Americano para Real Brasil\n\n");
+            Console.Write("Cotação do US$ em R$: ");
+            double quotation = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantia de compra de US$: ");
+            double dolars = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double reais = Conversion.CurrencyConversion(quotation, dolars);
+            Console.WriteLine("\nValor a ser pago: R$ " + reais.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
