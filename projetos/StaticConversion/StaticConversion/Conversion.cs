dc2cf0fde0042253a0951d1292df4cfebe81809e
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-08-06 23:11:52 -0300

    Refactoring class Conversion

--- projetos/StaticConversion/StaticConversion/Conversion.cs
+++ projetos/StaticConversion/StaticConversion/Conversion.cs
@@ -6,10 +6,10 @@ namespace StaticConversion
 {
     class Conversion
     {
-        public static double tax = 1.06;
+        public static double Tax = 1.06;
         public static double CurrencyConversion(double quotation, double dolars)
         {
-            return quotation * (dolars * tax);
+            return quotation * (dolars * Tax);
         }
     }
 }
