187f735602d84fcec6aed6f4c8a47ea4e933f0df
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-08-06 22:02:41 -0300

    Refactoring MetEstatico1, adding new file 'Calculadora.cs'

---
+++ projetos/MetEstatico1/MetEstatico1/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿
+namespace MetEstatico1
+{
+    internal class Calculadora
+    {
+        public double Pi = 3.14159;
+
+        public double Circunferencia(double r)
+        {
+
+            return 2 * r * Pi;
+        }
+
+        public double Volume(double r)
+        {
+
+            return 4 / 3 * Pi * Math.Pow(r, 3);
+        }
+    }
+}
