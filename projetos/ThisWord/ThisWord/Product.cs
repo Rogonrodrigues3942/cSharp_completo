325ff1923921abc1be5f772608c365e8878a218f
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-08-11 20:25:29 -0300

    Adding new project: ThisWord.

---
+++ projetos/ThisWord/ThisWord/Product.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+
+namespace Problem2
+{
+    class Product
+    {
+        public string ProdName;
+        public double Price;
+        public int Quantity;
+
+        public Product()
+        {
+            Quantity = 10;
+        }
+
+        public Product(string prodName, double price) : this()
+        {
+            ProdName = prodName;
+            Price = price;
+        }
+
+        public Product(string prodName, double price, int quantity  ): this(prodName, price) 
+        {
+            ProdName = prodName;
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Price * Quantity;
+        }
+
+        public void AdicionarProdutos(int quantity)
+        {
+            Quantity += quantity;
+        }
+        public void RemoverProdutos(int quantity)
+        {
+            Quantity -= quantity;
+        }
+
+        public override string ToString()
+        {
+            return "Produto: " + ProdName +
+                 ", \nR$ " + Price.ToString("F2", CultureInfo.InvariantCulture) +
+                 "\nQuantidade: " + Quantity +
+                "\nValor em estoque: " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ projetos/ThisWord/ThisWord/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Controle de Produto em Estoque - POO");
+
+            Console.WriteLine("\nInsira os dados do produto.\n\n");
+
+            Console.Write("Insira o nome do produto: ");
+            string prodName = Console.ReadLine();
+
+            Console.Write("Insira a quantidade: ");
+            int quantity = int.Parse(Console.ReadLine());
+
+            Console.Write("Insira o preço: ");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Product p = new Product
+            {
+                ProdName = prodName,
+                Price = price,
+                Quantity = quantity
+            };
+
+            Console.WriteLine($"\nDados do produto: {p}");
+
+            Console.Write("\nAdiciona novas unidades: ");
+            quantity = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(quantity);
+            Console.WriteLine($"\nDados do produto: {p}");
+
+
+            Console.Write("\nRemova unidades: ");
+            quantity = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(quantity);
+            Console.WriteLine($"\nDados do produto: {p}");
+        }
+    }
+}
