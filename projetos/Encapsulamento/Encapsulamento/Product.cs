22578234951dc4cd607cfd9bc16831dc659f7857
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-08-11 22:22:45 -0300

    Adding new project: Encapsulamento.

---
+++ projetos/Encapsulamento/Encapsulamento/Product.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+
+namespace Problem2
+{
+    class Product
+    {
+        private string _prodName;
+        private int _quantity;
+        private double _price;
+
+        public Product() { }
+
+        public Product(string prodName, double price, int quantity)
+        {
+            _prodName = prodName;
+            _price = price;
+            _quantity = quantity;
+        }
+
+        public void SetNome(string prodNome)
+        {
+
+            if (prodNome != null && prodNome.Length > 1)
+            {
+                _prodName = prodNome;
+            }
+        }
+        public string GetNome() { return _prodName; }
+
+        public double GetPrice() { return _price; }
+
+        public int GetQuantity() { return _quantity; }
+
+        public double ValorTotalEmEstoque()
+        {
+            return _price * _quantity;
+        }
+
+        public void AdicionarProdutos(int quantity)
+        {
+            _quantity += quantity;
+        }
+        public void RemoverProdutos(int quantity)
+        {
+            _quantity -= quantity;
+        }
+
+        public override string ToString()
+        {
+            return "Produto: " + _prodName +
+                 ", \nR$ " + _price.ToString("F2", CultureInfo.InvariantCulture) +
+                 "\nQuantidade: " + _quantity +
+                "\nValor em estoque: " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ projetos/Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,25 @@
+﻿using Problem2;
+using System;
+using System.Globalization;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nEncapsulamento - POO \n\n");
+
+            Product p = new Product("TV 32'", 999.99, 10);
+
+            Console.WriteLine(p);
+            Console.WriteLine("\n" + p.GetNome());
+
+            p.SetNome("TV 4K");
+
+            Console.WriteLine("\n" + p.GetNome());
+            Console.WriteLine("\n" + p.GetPrice());
+            Console.WriteLine("\n" + p.GetQuantity());
+        }
+    }
+}
