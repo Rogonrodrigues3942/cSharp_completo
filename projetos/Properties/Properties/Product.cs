3d80c90262906b0145fc5f5484a3e4bcb226a52f
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-08-12 09:20:57 -0300

    Adding new project: Propeties.

---
+++ projetos/Properties/Properties/Product.cs
@@ -0,0 +1,65 @@
+﻿using System.Globalization;
+
+namespace Properties
+{
+    class Product
+    {
+        private string _prodName;
+        private int _quantity;
+        private double _price;
+
+        public Product() { }
+
+
+        public Product(string prodName, double price, int quantity)
+        {
+            _prodName = prodName;
+            _price = price;
+            _quantity = quantity;
+        }
+
+        public string ProdName
+        {
+            get { return _prodName; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _prodName = value;
+                }
+            }
+        }
+
+        public double Price
+        {
+            get { return _price; }
+        }
+
+        public int Quantity
+        {
+            get { return _quantity; }
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return _price * _quantity;
+        }
+
+        public void AdicionarProdutos(int quantity)
+        {
+            _quantity += quantity;
+        }
+        public void RemoverProdutos(int quantity)
+        {
+            _quantity -= quantity;
+        }
+
+        public override string ToString()
+        {
+            return "Produto: " + _prodName +
+                 ", \nR$ " + _price.ToString("F2", CultureInfo.InvariantCulture) +
+                 "\nQuantidade: " + _quantity +
+                "\nValor em estoque: " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ projetos/Properties/Properties/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nEncapsulamento - POO \n\n");
+
+            Product p = new Product("TV 32'", 500.00, 10);
+
+            Console.WriteLine(p);
+            Console.WriteLine("\n" + p.ProdName);
+
+            p.ProdName = "TV 4K";
+
+            Console.WriteLine("\n" + p.ProdName);
+            Console.WriteLine("\n" + p.Price);
+            Console.WriteLine("\n" + p.Quantity);
+        }
+    }
+}
