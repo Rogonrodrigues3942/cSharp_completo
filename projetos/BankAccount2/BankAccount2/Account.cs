c0648c822d2959faa777a309600fd8e8e28329e5
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-08-16 21:55:15 -0300

    Adding exercise, new project BanckAccount2

---
+++ projetos/BankAccount2/BankAccount2/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace BankAccount2
+{
+    class Account
+    {
+        public int AccountNumber {  get; private set; }
+        public string Customer { get; set; }
+
+        public double Balance {  get; private set; }
+
+        public Account(int accountNumber, string customer)
+        {
+            AccountNumber = accountNumber;
+            Customer = customer;
+        }
+        public Account(int accountNumber, string customer, double initialDeposit):this(accountNumber, customer)
+        {
+           
+            Deposit(initialDeposit);
+        }
+
+        public void Deposit(double money)
+        {
+            Balance += money;
+        }
+
+        public void Plunder(double money)
+        {
+            Balance -= money + 5.00;
+        }
+
+        public override string ToString()
+        {
+            return "Número da conta: " + AccountNumber 
+                + "\nNome do cliente: " + Customer 
+                + "\nSaldo: " + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ projetos/BankAccount2/BankAccount2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace BankAccount2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nConta bancária: depósito e saque.\n\n");
+            Console.Write("Digite o número da conta: ");
+            int accountNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("\nDigite o nome do correntista: ");
+            string customer = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (S/N): ");
+            char depositoInicial = char.Parse(Console.ReadLine());
+
+            Account conta; 
+
+            if ( depositoInicial == 'S' || depositoInicial == 's')
+            {
+                Console.Write("\nQual o valor do depósito inicial: ");
+                double deposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Account(accountNumber, customer, deposit);
+            }
+            else
+            {
+                conta = new Account(accountNumber, customer );
+            }
+
+
+            Console.Write("\nQual o valor de depósito: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposit(amount);
+
+            Console.Write("\nQual o valor do saque: ");
+            amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Plunder(amount);
+
+            Console.WriteLine(conta);
+        }
+    }
+}
