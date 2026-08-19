091cc352aa182a2a58a0f4262249fca6b73ef196
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-08-19 18:40:54 -0300

    Adding new project. BankAccount3

---
+++ projetos/BankAccount3/BankAccount3/Account.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace BankAccount3
+{
+     class Account
+    {
+        public int AccountNumber {  get; private set; }
+        public string Name { get; set; }
+        public double Balance {  get; private set; }
+
+        public Account(int  accountNumber, string name)
+        {
+            AccountNumber = accountNumber;
+            Name = name;
+        }
+
+        public Account(int accountNumber, string name, double balance) : this(accountNumber, name)
+        {
+            Balance = balance;
+        }
+
+
+        public override string ToString()
+        {
+            return "\nNúmero da conta: " + AccountNumber
+                +"\nNome do correntista: " + Name
+                + "\nSaldo da conta:  R$ " + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ projetos/BankAccount3/BankAccount3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankAccount3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
