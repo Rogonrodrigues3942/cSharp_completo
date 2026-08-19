d6e8257dfc47e25cff6a7ecea64a72fa7a267297
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-08-19 20:08:44 -0300

    Adding new project, about vector.

---
+++ projetos/VectorClass1/VectorClass1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace VectorClass1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vetores - parte 1\n\n");
+
+            Console.Write("Digite o tamanho do vetor: ");
+            int n = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Digite o valor da posição: ");
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                //Console.WriteLine(vect[i]);
+            }
+            Console.WriteLine();
+            //Console.WriteLine(vect);
+
+            double sum = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += vect[i];
+            }
+
+            double avg = sum / n;
+            Console.WriteLine($"A média é: {avg:F2}");
+        }
+    }
+}
