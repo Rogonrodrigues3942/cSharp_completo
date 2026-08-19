b29f910e59f190683a39b76884e0a7cb08a044fb
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-08-19 20:29:01 -0300

    Adding new project, about vector. Reinforcement exercise.

---
+++ projetos/VectorClass1RF/VectorClass1RF/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace VectorClass1RF
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicio de Reforço\n\n");
+
+            Console.Write("Digite o tamanho do vetor:  ");
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++) {
+
+                Console.Write($"Digite o número da {i +1}ª posição:  ");
+                vect[i] = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            }
+
+            double sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += vect[i];
+            }
+
+            double avg = sum / n;
+            Console.WriteLine($"Média: {avg:F2}");
+
+
+        }
+    }
+}
