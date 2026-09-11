664fbcc86ce81cf4a6ff37cf835e50f4eb56bbfb
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-09-11 19:13:43 -0300

    Inserting new files about foreach studying.

---
+++ projetos/ForEachUse/ForEachUse/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ForEachUse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nforeach\n\n");
+
+            string[] names = new string[] { "Rogério", "Rafael", "Samuel" };
+
+            Console.WriteLine("\n ---- Usando for para repetição ---- \n ");
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            Console.WriteLine("\n ---- Usando foreach para repetição ---- \n ");
+            foreach( string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ projetos/ForEachUse2/ForEachUse2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ForEachUse2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n foreach - using 2\n\n");
+
+            int [] sequencia = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Console.WriteLine("\n\n****** Usando for para laço de repetição ******\n");
+            for (int i = 0; i < sequencia.Length; i++)
+            {
+                Console.WriteLine(sequencia[i]);
+            }
+
+            Console.WriteLine("\n\n****** Usando foreach para laço de repetição ******\n");
+            foreach(int numero in sequencia)
+            {
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}
