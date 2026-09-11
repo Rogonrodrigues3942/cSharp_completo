8f86b2c816aece30c4253f26ef78fce9a9335484
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-09-11 19:12:40 -0300

    Inserting projetos/OutModifier/, about estudying modifier.

---
+++ projetos/OutModifier/OutModifier/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OutModifier
+{
+    internal class Calculator
+    {
+        public static void Triple (int origin, out int result)
+        {
+            result = origin * 3;
+        }
+    }
+}
---
+++ projetos/OutModifier/OutModifier/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OutModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nout modifier\n");
+
+            int a = 10;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine("\n" + triple);
+        }
+    }
+}
