1ebcdc184574f68c53ae4b23a94bfe6bc476df4d
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-09-11 18:04:57 -0300

    Insertinf RefModifier project.

---
+++ projetos/RefModifier/RefModifier/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace RefModifier
+{
+    internal class Calculator
+    {
+        public static void Triple(ref int x)
+        {
+            x *= 3;
+        }
+    }
+}
---
+++ projetos/RefModifier/RefModifier/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RefModifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nref modifier\n\n");
+
+            int a = 10;
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+        }
+    }
+}
