d9891b2b41fbf416f2cc9c41858a4a772aa717c8
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-09-12 15:14:25 -0300

    nserting new project, the theme about  "List".

---
+++ projetos/ListClass1/ListClass1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace ListClass1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aula 1 - Listas\n\n");
+
+            //Instanciada uma lista vazia.
+            List<string> list = new List<string>();
+
+            //Instanciada uma lista com elementos.
+            List<string> list2 = new List<string>{"Rogério", "Rafael" };
+
+            foreach (string nome in list2)
+            {
+                Console.WriteLine(nome);
+            }
+
+        }
+    }
+}
