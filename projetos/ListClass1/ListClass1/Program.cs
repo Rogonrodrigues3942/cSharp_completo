95bae72d23baaed684adbd09238191d6d95bd64c
Rogonrodrigues3942 <rogonrodrigues@gmail.com>
2026-09-12 15:30:57 -0300

    Updatinf and insertinf files about LIST.

--- projetos/ListClass1/ListClass1/Program.cs
+++ projetos/ListClass1/ListClass1/Program.cs
@@ -19,6 +19,8 @@ namespace ListClass1
                 Console.WriteLine(nome);
             }
 
+
+
         }
     }
 }
---
+++ projetos/ListClass2/ListClass2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ListClass2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nAula 2 - List \n\n");
+
+            //Instanciando uma lista
+            List <string> list = new List<string>();
+
+            //Inserindo dados na lista
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Anna");
+
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            //inserindo item em uma posição determinada.
+            Console.WriteLine("\n");
+            list.Insert(2, "Marco");
+            list.Insert(4, "Rafael");
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\n\nTamanho da lista: " + list.Count());
+        }
+    }
+}
